517941e61ebfee831c51cbdec56e9e816648f52b
starnakin.desktop <starnakin2@gmail.com>
2020-07-03 16:15:37 +0200

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+import moviepy.editor
+
+def get_file_name(file):
+    return str(file).split(".")[0]
+
+def convertor_replyer(file, max_time):
+    if str(file).endswith(".mp4"):
+        clip = moviepy.editor.VideoFileClip(file)
+        clip_list = []
+        i=0
+        while i+clip.duration < max_time:
+            clip_list.append(clip)
+            i+=clip.duration
+            print((i/max_time)*100,"%")
+        print("100%")
+        moviepy.editor.concatenate_videoclips(clip_list).write_videofile(get_file_name(file)+"[10].mp4", codec='libx264')
+    if str(file).endswith(".mp3"):
+        clip = moviepy.editor.AudioFileClip(file)
+        clip_list = []
+        i=0
+        while i+clip.duration < max_time:
+            clip_list.append(clip)
+            i+=clip.duration
+            print((i/max_time)*100,"%")
+        print("100%")
+        moviepy.editor.concatenate_audioclips(clip_list).write_videofile(get_file_name(file)+"[10].mp3", codec='libx264')
+
+#def convertor_mp3_to_mp4(file):
+#    moviepy.editor.VideoFileClip(file).audio.write_audiofile(get_file_name+".mp3")
+
+def convertor_mp4_to_mp3(file):
+    moviepy.editor.VideoFileClip(file).audio.write_audiofile(str(get_file_name)+".mp3")
+
+convertor_mp4_to_mp3("3s.mp4")
